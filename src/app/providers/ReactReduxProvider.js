c2f44a6c4567c497f2f3b2faf5fc83a5e1560376
79r <riski.web.id@zoho.com>
2020-11-28 21:10:29 +0700

    add configuration redux-persist

--- src/app/providers/ReactReduxProvider.js
+++ src/app/providers/ReactReduxProvider.js
@@ -1,8 +1,18 @@
 import { Provider } from "react-redux";
-import { store } from "../redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from '../redux/configureStore';
+// import { store } from "../redux/store";
+
+
+const { store, persistor } = configureStore();
+
 
 const ReactReduxProvider = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      {children}
+    </PersistGate>
+  </Provider>
 );
 
 export default ReactReduxProvider;
---
+++ src/app/redux/configureStore.js
@@ -0,0 +1,38 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import rootReducer from "./reducers/rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const middlewares = [thunk];
+let devtools = (x) => x;
+
+if (
+  process.env.NODE_ENV !== "production" &&
+  process.browser &&
+  window.__REDUX_DEVTOOLS_EXTENSION__
+) {
+  devtools = window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const configureStore = () => {
+  let store = createStore(
+    persistedReducer,
+    compose(applyMiddleware(...middlewares), devtools)
+  );
+  let persistor = persistStore(store);
+  return {
+    store,
+    persistor
+  }
+}
+
+export default configureStore;
