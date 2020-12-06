ee459a54a4a3415771748530e964924786ff8c66
79r <riski.web.id@zoho.com>
2020-12-06 23:56:34 +0700

    Add fake-db

---
+++ src/fake-db/layout.js
@@ -0,0 +1,18 @@
+const layoutDB = {
+  layouts: [
+    {
+      id: 1,
+      name: "LAYOUT_1",
+    },
+    {
+      id: 2,
+      name: "LAYOUT_2",
+    },
+    {
+      id: 3,
+      name: "LAYOUT_3",
+    },
+  ],
+};
+
+export default layoutDB;
---
+++ src/fake-db/widget.js
@@ -0,0 +1,44 @@
+const widgetDB = {
+  widgets: [
+    {
+      id: 1,
+      name: "Header1",
+      type: 1,
+    },
+    {
+      id: 2,
+      name: "Blog1",
+      type: 2,
+    },
+    {
+      id: 3,
+      name: "Label1",
+      type: 3,
+    },
+    {
+      id: 4,
+      name: "Label2",
+      type: 3,
+    },
+  ],
+  widgetType: [
+    {
+      id: 1,
+      name: "Header",
+    },
+    {
+      id: 2,
+      name: "Blog",
+    },
+    {
+      id: 3,
+      name: "Label",
+    },
+    {
+      id: 4,
+      name: "PageList",
+    },
+  ],
+};
+
+export default widgetDB;
